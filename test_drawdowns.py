5e762dca39d7b182b1fef602d7c4cd8046ceeb5f
fhashim <f.hashim@outlook.com>
2022-03-09 17:09:54 +0500

    updated

--- test_drawdowns.py
+++ test_drawdowns.py
@@ -2,6 +2,9 @@ from mvn_historical_drawdowns import historical_drawdowns
 
 from Functions.mvn_historical_returns import historical_returns
 
+from Functions.mvn_historical_returns_dollar import \
+    historical_returns_dollar
+
 asset_code = 'SPY US'
 price_type = 'GTR'
 period_start = ['2021-12-31', '2020-12-31', '2019-12-31', '2018-12-31',
@@ -35,14 +38,17 @@ results = historical_returns('SPY US', 'PR', None,
                              ['1Y', '2W', '6M', '3Q', '95D',
                               'Inception', '30Y', '50Y'],
                              [None, None, None, None, None, None,
-                              None, None],
-                             None, None
+                              None, None]
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['1Y', '1Y', '1Y', '1Y', '1Y'],
+                             ['2Y', '3Y', '5Y', '6Q', '3M']
                              )
 
 results = historical_returns('QQQ US', 'PR', None,
                              ['1Y', '1Y', '1Y', '1Y', '1Y'],
-                             ['2Y', '3Y', '5Y', '6Q', '3M'],
-                             None, None
+                             ['2Y', '3Y', '5Y', '6Q', '3M']
                              )
 
 results = historical_returns('SPY US', 'PR', None,
@@ -50,13 +56,61 @@ results = historical_returns('SPY US', 'PR', None,
                               'Inception', '30Y', '50Y'],
                              [None, None, None, None, None, None, None,
                               None],
-                             'NA', None
+                             normalisation_freq=None
                              )
 
 results = historical_returns('SPY US', 'PR', None,
                              ['20Y'],
                              [None],
-                             None, '2W'
+                             compounding_freq='2W'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             compounding_freq='2M'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             compounding_freq='2Q'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             compounding_freq='50D'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             compounding_freq='3Y'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             '6M', '2W'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             '3Q', '2M'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             '5Y', '2Q'
+                             )
+
+results = historical_returns('SPY US', 'PR', None,
+                             ['20Y'],
+                             [None],
+                             '30W', '50D'
                              )
 
 results = historical_returns('SPY US', 'PR', None,
@@ -65,12 +119,64 @@ results = historical_returns('SPY US', 'PR', None,
                              '10Y', '3Y'
                              )
 
-asset_code = 'SPY US'
-price_type = 'PR'
-period_start = '3Q'
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['1Y', '2W', '6M', '3Q', '95D',
+                                     'Inception', '30Y', '50Y'],
+                                    [None, None, None, None, None, None,
+                                     None, None], 1000, None
+                                    )
 
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['1Y', '1Y', '1Y', '1Y', '1Y'],
+                                    ['2Y', '3Y', '5Y', '6Q', '3M'],
+                                    1000, None
+                                    )
 
-asset_code = 'SPY US'
-price_type = 'PR'
-period_start = '20Y'
-comp_freq = '2W'
+results = historical_returns_dollar('QQQ US', 'PR', None,
+                                    ['1Y', '1Y', '1Y', '1Y', '1Y'],
+                                    ['2Y', '3Y', '5Y', '6Q', '3M'],
+                                    1000, None
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['1Y', '2W', '6M', '3Q', '95D',
+                                     'Inception', '30Y', '50Y'],
+                                    [None, None, None, None, None, None,
+                                     None, None], 1000, '1Y', '1Y'
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['20Y'],
+                                    [None], 1000, compounding_freq='2W'
+
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['20Y'],
+                                    [None], 1000, '6M', '2W'
+
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['20Y'],
+                                    [None], 1000, '3Q', '2M'
+
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['20Y'],
+                                    [None], 1000, '5Y', '2Q'
+
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['20Y'],
+                                    [None], 1000, '30W', '50D'
+
+                                    )
+
+results = historical_returns_dollar('SPY US', 'PR', None,
+                                    ['20Y'],
+                                    [None], 1000, '10Y', '3Y'
+
+                                    )
